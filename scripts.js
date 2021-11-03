acb463b68d00c31ad43adf6d27e85a8f80dd3051
Justin Hamilton <jwhamilton99@gmail.com>
2021-11-03 16:41:38 -0400

    added site files

---
+++ scripts.js
@@ -0,0 +1,121 @@
+let characters = ["&nbsp",".",":","-","=","+","*","#","%","@"];
+
+function getRawText() {
+	var text = document.getElementById("output").innerHTML;
+	text = text.replace(/&nbsp;/gm, " ");
+	text = text.replace(/<br>/gm, "\n");
+	return text;
+}
+
+function copyText() {
+	navigator.clipboard.writeText(getRawText());
+}
+
+function saveText() {
+	var blob = new Blob([getRawText()],{
+		type:"text/plain;charset=utf-8"
+	});
+	window.location = (window.webkitURL.createObjectURL(blob));
+
+}
+
+function updateOutputColors() {
+	var checked = document.getElementById("invert").checked;
+	document.getElementById("outputContainer").style.backgroundColor = (checked) ? "black" : "white";
+	document.getElementById("output").style.color = (checked) ? "white" : "black";
+}
+
+function displayImage(values) {
+	output = ""
+	for(row of values) {
+		output = output + (row.map(getCharacter).toString())+"<br>";
+	}
+	output = output.replace(/,/g," ");
+	return output;
+}
+
+function getCharacter(num) {
+	return characters[num];
+}
+
+function processRow(canvas, ctx, y) {
+	row = []
+	for(var x = 0; x < canvas.width; x++) {
+		let data = ctx.getImageData(x, y, canvas.width, canvas.height).data;
+		avg = (data[0]+data[1]+data[2])/3;
+		row.push(Math.floor((avg*characters.length)/256));
+	}
+	return row;
+}
+
+function processImage(canvas, ctx) {
+	console.log("processing image");
+	var output = [];
+	for(var y = 0; y < canvas.height; y++) {
+		row = processRow(canvas, ctx, y);
+		output.push(row);
+	}
+	return displayImage(output);
+}
+
+function createCanvas(image) {
+	console.log("creating canvas");
+	var canvas = document.createElement("canvas");
+	canvas.width = document.getElementById("width").value;
+	canvas.height = document.getElementById("height").value;
+	
+	let ctx = canvas.getContext('2d');
+	ctx.drawImage(image, 0,0, document.getElementById("width").value, document.getElementById("height").value);
+	
+	return processImage(canvas, ctx);
+}
+
+function createImage(e) {
+	var image = new Image();
+	image.src = e.target.result;
+	
+	image.onload = function() {
+		if((document.getElementById("width").value == "" && document.getElementById("height").value == "") || document.getElementById("imagePreview").src != e.target.result) {
+			document.getElementById("width").value = image.width;
+			document.getElementById("height").value = image.height;
+			document.getElementById("controls").style.display = "block";
+		}
+		
+		document.getElementById("imagePreview").src = e.target.result;
+		
+		var output = createCanvas(image);
+		document.getElementById("output").style.fontSize = document.getElementById("fontsize").value+"px";
+		document.getElementById("output").innerHTML = output;
+		document.getElementById("status").innerHTML = "";
+		console.log("finished!");
+	}
+}
+
+function updateFontSize() {
+	document.getElementById("output").style.fontSize = document.getElementById("fontsize").value+"px";
+}
+
+
+
+function loadImage() {
+	let reader = new FileReader();
+	
+	reader.addEventListener("load", createImage);
+	
+	if(document.getElementById("file").files.length == 0) {
+		alert("please select an image");
+		return;
+	}
+	
+	document.getElementById("width").value = Math.max(1,document.getElementById("width").value);
+	document.getElementById("height").value = Math.max(1,document.getElementById("height").value);
+	document.getElementById("fontsize").value = Math.max(1,document.getElementById("fontsize").value);
+	
+	document.getElementById("status").innerHTML = "processing...";
+	
+	updateOutputColors();
+	
+	file = (document.getElementById("file").files[0]);
+	
+	reader.readAsDataURL(file);
+}
